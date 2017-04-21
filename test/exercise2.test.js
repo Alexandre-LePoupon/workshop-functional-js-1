096cc215e273679405ec97e579315a6624a3f4c4
Ocarinos <alexlepoupon@gmail.com>
2017-04-21 10:04:33 +0200

    Exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -3,10 +3,10 @@ const reverseYolo = require('../src/exercise2').reverseYolo;
 
 describe('GIVEN function reverseYolo', function() {
 
-  it('WHEN my mystery input THEN my wonderful output', function() {
+  it.only('WHEN my mystery input THEN my wonderful output', function() {
 
     // Your future job begins here ...
-    let whereIsMyMind = '???';
+    let whereIsMyMind = [ 'T', 'R', 'O', 'N', 'K', 'G', '3', '3', '0', [7], [4], [2], [2], [1], [1], [0], [0]];
 
     expect(reverseYolo(whereIsMyMind)).to.eql([ 'T', 'R', 'O', 'N', 'K', 'G', '3', '3', '0', 7, 4, 2, 2, 1, 1, 0, 0 ]);
   });
