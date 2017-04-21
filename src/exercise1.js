fd6f18e86f058458df1b902e423d6219145e0a2b
Ocarinos <alexlepoupon@gmail.com>
2017-04-21 10:49:36 +0200

    New test for exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -9,10 +9,15 @@ const zip = (input) => {
 		return [];
 	} else {
 		var tmp = _.unzip(input);
+		var ret = new Array();
 		return _.forEach(tmp, function(index) {
-			return _.without(index, undefined);
+			tmp2 = index.filter(function(n){ return n != undefined })
+			// tmp2 = _.without(index, undefined);
+			console.log(tmp2);
+			ret.push(tmp2);
 
 		});
+		return ret;
 	}
 }
 
